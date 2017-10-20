0e0b258639212d14ed9940ad56e2b0c42983c9a5
Ana <ana.bastos881@gmail.com>
2017-10-20 11:33:28 -0200

    Getting types

--- handlers/lexicalAnalysis.js
+++ handlers/lexicalAnalysis.js
@@ -10,41 +10,89 @@ import errorHelper from '../utils/errorHelper'
 const splitReaderToLines = content => content.toString().split('\n')
 
 // to_char()
-const toChar = content => content.map(line => line.split(''))
+const toChar = content => content.map(line => line.split(' '))
 
-// ler_simbolo() 
-//Gera token
-const readSimbol = (item, line, table, reservedItems) => item.map((char, index) => {
-    const typeOfChar = getTypeOfToken(char)
-    const tstIndex = table.actionTable('I', char)
-    typeOfChar || errorHelper('Léxico', line, `Simbolo ${item}  inexistente no alfabeto.`)
-    return makeTokenObj(char, typeOfChar, tstIndex, {line, index})
-})
 
 const readLines = arr => {
     const table = tst.hashTable();
     return getReservedItens().then(
-        reservedItems => arr.map((char, line) => readSimbol(char, line, table, reservedItems.toString().split('\n')))
+        reservedItems => {
+            const formatReserved = reservedItems.toString().split('\n')
+            return arr.map((line, lineIndex) => {
+                const filteredline = filterLine(line)
+                return readSimbol(filteredline, lineIndex, table, formatReserved)})
+        }
     ).catch(console.error)
 }
 
+// ler_simbolo() 
+//Gera token
+const readSimbol = (item, line, table, reservedItems) => item.map((char, index) => {
+    const typeOfChar = getTypeByRegex(char)
+    const tstIndex = table.actionTable('I', char)
+    const type = checkIfReserved(char, reservedItems) || typeOfChar
+
+    const accepted = type !== null;
+    // accepted 
+        // ? console.log(`Cadeia aceita ${item}`)
+        // : console.log(`Cadeia rejeitada ${item}`)
+
+    // accepted || errorHelper('Léxico', line, `Simbolo ${item}  inexistente no alfabeto.`)
+    return makeTokenObj(char, type, tstIndex, {line, index})
+})
+
+
 // Pega o tipo do token por regex
-const getTypeOfToken = item => {
+const getTypeByRegex = item => {
     const checkedByRegex = Object.keys(regexObj).reduce((acc, regex) => {
         return item.match(regex) 
             ? regexObj[regex] 
             : acc
     }, null)
     
-    const type = checkIfReserved(checkedByRegex)
-
-    const accepted = type !== null;
-    accepted 
-        ? console.log(`Cadeia aceita ${item}`)
-        : console.log(`Cadeia rejeitada ${item}`)
-
-    return type
+    return checkedByRegex
 }
+
+const filterLine = line => pipe(
+    filterSpaces,
+    removeTabsAndLF,
+    filterSingleComments
+)(line)
+
+const removeTabsAndLF = arr => arr.filter(token => token.type !== ('LF' || 'tab'))
+
+const filterSpaces = arr => arr.filter((item) => item !== '');
+// const filterSpaces = arr => arr.filter((item, i, arr) => arr[i - 1] !== ' ' || item !== ' ');
+
+const filterSingleComments = arr => {
+    const index = arr.indexOf('//')
+    return !!~index ? arr.splice(0, index) : arr
+}
+
+const checkIfReserved = (value, reserved) => {
+    var indexToSplit = reserved.indexOf('-=');
+    
+    var words = reserved.slice(0, indexToSplit);
+    var simbols = reserved.slice(indexToSplit);
+    
+    return checkIfReservedSimbol(value, simbols) || checkIfReservedWord(value, words)
+}
+
+const getReservedItens = () => {
+    return promisify(fs.readFile)('./utils/reservedWords.txt')
+}
+
+const checkIfReservedWord = (value, words) => words.includes(value) ? 'WORD' : null
+const checkIfReservedSimbol = (value, words) => words.includes(value) ? 'SIMBOL' : null
+
+const makeTokenObj = (item, type, tstIndex, position) => ({ type: type, value: item, tst: tstIndex, position: `${position.line}:${position.index}`})
+
+const lexan = file => pipe(
+    splitReaderToLines,
+    toChar,
+    readLines)(file)
+
+export default lexan
 
 
 // todo
@@ -54,9 +102,12 @@ const getTypeOfToken = item => {
 // errorHelper('Léxico', line, `Comentários sem fechamento..`)
 // errorHelper('Léxico', line, `Constante alfanumerica sem encerramento..`)
 
-// considerar só um espaço
-// considerar comentarios
+//[x] considerar só um espaço
+//[x] considerar comentarios
+// considerar comentarios multilinha
 // abrir e fechar de delimitadores
+//[x] checar se num reservado
+//[x] checar se simbolo reservado
 
 // definicao de constantes
 // NUMBER
@@ -64,36 +115,3 @@ const getTypeOfToken = item => {
 // - IDENT
 // - EOF
 // - FLOAT
-
-
-const checkIfReservedWord = value => {
-    // palavra reservada ou simbolo reservado
-    return value
-}
-
-const checkIfSimbol = value => {
-    // palavra reservada ou simbolo reservado
-    return value
-}
-
-const removeTabsAndLF = arr => arr.filter(token => token.type !== ('LF' || 'tab'))
-
-const filterSpaces = arr => arr.filter(token => token.type !== 'b')
-
-const getReservedItens = () => {
-    return promisify(fs.readFile)('./utils/reservedWords.txt')
-}
-
-// a) Item Léxico: cadeia de caracteres referente ao token.
-// b) Valor numérico do inteiro.
-// c) Valor numérico do tipo float.
-// d) O conteúdo armazenado na TST referente ao índice retornado. Não tem utilidade
-// para fins de compilador. Apenas para verificação do funcionamento do Lexan.
-const makeTokenObj = (item, type, tstIndex) => ({ type: type, value: item, tst: tstIndex})
-
-const lexan = file => pipe(
-    splitReaderToLines,
-    toChar,
-    readLines)(file)
-
-export default lexan
--- utils/lexicalRegex.js
+++ utils/lexicalRegex.js
@@ -2,6 +2,10 @@ export default {
     '\t': 'tab', //      Tabulador
     '[ |\s]': 'b', //         Espaço
     '\n': 'LF', //         LineFeed
-    '[0-9]': 'd', //     Numero
+    '[0-9]': 'd', //     Digito
     '[a-zA-Z_]': 'l', // A-Z e Underscore
+    '[a-zA-Z][a-zA-Z0-9]*': 'IDENT',
+    '"[a-zA-Z][a-zA-Z0-9]*"': 'ALPHA',
+    '[-+]?[0-9]': 'NUMBER',
+    '[-+]?[0-9]*\.?[0-9]': 'FLOAT'
 }
